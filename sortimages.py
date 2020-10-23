5b0ac94f9f9e7d87b83fcda709aaae5f8eb868ef
System Administrator <root@campus-034-070.ucdavis.edu>
2020-10-23 16:55:14 -0700

    dataset v2

--- sortimages.py
+++ sortimages.py
@@ -8,14 +8,16 @@ if len(sys.argv) != 3:
 filelist = open(sys.argv[1],'r').readlines()
 startindex = int(sys.argv[2])
 
-one_train_dir = '/Users/dhawal/Desktop/aqi/images.nosync/acapp_data/training_images/one_train/'
-mul_trains_dir = '/Users/dhawal/Desktop/aqi/images.nosync/acapp_data/training_images/mul_trains/'
-no_train_dir = '/Users/dhawal/Desktop/aqi/images.nosync/acapp_data/training_images/no_train/'
-night_train_dir = '/Users/dhawal/Desktop/aqi/images.nosync/acapp_data/training_images/night_train/'
-night_no_train_dir = '/Users/dhawal/Desktop/aqi/images.nosync/acapp_data/training_images/night_no_train/'
+one_train_dir = '/Users/dhawal/Desktop/aqi/images/acapp_data/training_images/one_train/'
+mul_trains_dir = '/Users/dhawal/Desktop/aqi/images/acapp_data/training_images/mul_trains/'
+no_train_dir = '/Users/dhawal/Desktop/aqi/images/acapp_data/training_images/no_train/'
+night_train_dir = '/Users/dhawal/Desktop/aqi/images/acapp_data/training_images/night_train/'
+night_no_train_dir = '/Users/dhawal/Desktop/aqi/images/acapp_data/training_images/night_no_train/'
 
 ch = 'n'
 i = startindex
+
+saves = []
 
 while ch!='q':
 	print(i)
@@ -25,15 +27,31 @@ while ch!='q':
 	cv2.waitKey(1)
 	name = line[0]
 	if ch == '1':
-		cv2.imwrite(one_train_dir+name+'.jpg',img)
+		# cv2.imwrite(one_train_dir+name+'.jpg',img)
+		saves.append([one_train_dir+name+'.jpg',img])
+		i += 1
 	elif ch == '2':
-		cv2.imwrite(mul_trains_dir+name+'.jpg',img)
+		# cv2.imwrite(mul_trains_dir+name+'.jpg',img)
+		saves.append([mul_trains_dir+name+'.jpg',img])
+		i += 1
 	elif ch == '3':
-		cv2.imwrite(no_train_dir+name+'.jpg',img)
+		# cv2.imwrite(no_train_dir+name+'.jpg',img)
+		saves.append([no_train_dir+name+'.jpg',img])
+		i += 1
 	elif ch == '4':
-		cv2.imwrite(night_train_dir+name+'.jpg',img)
+		# cv2.imwrite(night_train_dir+name+'.jpg',img)
+		saves.append([night_train_dir+name+'.jpg',img])
+		i += 1
 	elif ch == '5':
-		cv2.imwrite(night_no_train_dir+name+'.jpg',img)
-	i += 1
+		# cv2.imwrite(night_no_train_dir+name+'.jpg',img)
+		saves.append([night_no_train_dir+name+'.jpg',img])
+		i += 1
+	elif ch == 'd':
+		i -= 1
+		del saves[-1]
+	else:
+		i += 1
+	# elif 
 	ch = readchar.readchar()
 cv2.destroyAllWindows()
+[cv2.imwrite(l[0],l[1]) for l in saves]
